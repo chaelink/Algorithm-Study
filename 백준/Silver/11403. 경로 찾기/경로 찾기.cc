8d722ec0e4695b8d094b11904940079ebef77cc7
권채린 <92051742+chaelink@users.noreply.github.com>
2024-09-01 14:19:31 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main () {
+    int n;
+    cin>>n;
+    vector<vector<int>> adj(n, vector<int>(n));
+    
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin>>adj[i][j];
+        }
+    }
+    
+    for(int k=0; k<n; k++) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(adj[i][k] && adj[k][j])
+                    adj[i][j]=1;
+            }
+        }
+    }
+    
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+    
+}
