9f9c3c5eceb0fac756346a2a8679c29066180b2b
권채린 <92051742+chaelink@users.noreply.github.com>
2024-09-24 12:18:13 +0900

    [Silver IV] Title: 로프, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -6,16 +6,16 @@ int main() {
     int n;
     cin>>n;
     vector<int> lop(n,0);
-    vector<int> w(n,0);
+    int result=0;
     for(int i=0; i<n; i++) {
         cin>>lop[i];
     }
     sort(lop.begin(), lop.end(), greater<int>());
     
     for(int i=0; i<n; i++) {
-        w[i] = lop[i]*(i+1);
+        result = max(result,lop[i]*(i+1) );
     }
-    sort(w.begin(), w.end(), greater<int>());
-    cout<<w[0];
+    
+    cout<<result;
     return 0;
 }
