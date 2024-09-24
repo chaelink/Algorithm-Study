4eb08e8676e8326eef73d9494d315a5e07ccc9c8
권채린 <92051742+chaelink@users.noreply.github.com>
2024-09-24 11:24:39 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  
+    int n;
+    cin>>n;
+    vector<int> a(n,0);
+    vector<int> b(n,0);
+    for(int i=0; i<n; i++) {
+        cin>>a[i];
+    }
+    for(int i=0; i<n; i++) {
+        cin>>b[i];
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end(), greater<int>());
+    
+    int result=0;
+    
+    for(int i=0; i<n; i++) {
+        result += a[i]*b[i];
+    }
+    cout<<result;
+}
