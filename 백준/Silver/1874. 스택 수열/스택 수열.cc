d8c221db64cad6df60178c333a95d6687d4a4685
권채린 <92051742+chaelink@users.noreply.github.com>
2025-03-01 11:25:07 +0900

    [Silver II] Title: 스택 수열, Time: 20 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,39 +1,46 @@
-#include<iostream>
-#include<stack>
-#include<vector>
+#include <iostream>
+#include <stack>
+#include <vector>
 using namespace std;
+
 int main() {
-    vector<char> resultv;
-    stack<int> s;
-    int num =1;
-    string result = "t";
-    
+    ios::sync_with_stdio(false);  // C 스타일 입출력과의 동기화 해제
+    cin.tie(NULL);  // cin 성능 개선
+    cout.tie(NULL); // cout 성능 개선
+
     int n;
-    cin>>n;
-    
-    for(int i=0;i<n;i++) {
-        int x;
-        cin>>x;
-        while(num<=x) {
-            s.push(num);
-            num++;
-            resultv.push_back('+');
+    cin >> n;
+
+    stack<int> s;
+    vector<char> result;  // 연산 순서 기록
+
+    int current = 1;  // 1부터 push 시작
+
+    for (int i = 0; i < n; i++) {
+        int target;
+        cin >> target;
+
+        // 필요한 숫자까지 push
+        while (current <= target) {
+            s.push(current++);
+            result.push_back('+');
         }
-        
-        if(s.top()==x) {
+
+        // 스택 top이 목표 숫자와 같아야 pop 가능
+        if (s.top() == target) {
             s.pop();
-            resultv.push_back('-');
+            result.push_back('-');
+        } else {
+            // 순서 맞출 수 없음
+            cout << "NO\n";
+            return 0;  // 프로그램 즉시 종료
         }
-        else {
-            cout<<"NO";
-            result="f";
-            break;
-        } 
     }
 
-    if(result=="t") {
-        for(int i=0;i<resultv.size();i++)
-            cout<<resultv[i]<<'\n';
+    // 연산 기록 한꺼번에 출력
+    for (char op : result) {
+        cout << op << '\n';
     }
+
     return 0;
 }
