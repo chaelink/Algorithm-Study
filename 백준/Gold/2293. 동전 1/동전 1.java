e9fb9b417b103034ad4eec0fe6d5ff7353d7940b
권채린 <92051742+chaelink@users.noreply.github.com>
2025-03-28 11:25:21 +0900

    [Gold IV] Title: 동전 1, Time: 112 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        int[] dp = new int[k + 1];
+        
+        dp[0] = 1;  // DP 초기값은 반드시 1로 설정해야 합니다.
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for(int j = arr[i]; j <= k; j++) {
+                dp[j] += dp[j - arr[i]];
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+}
