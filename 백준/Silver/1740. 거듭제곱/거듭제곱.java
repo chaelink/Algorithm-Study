4e83c0010fd41e14f685aa8b53dbb374352b9bbb
권채린 <92051742+chaelink@users.noreply.github.com>
2025-05-01 17:35:57 +0900

    [Silver III] Title: 거듭제곱, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/1740. 거듭제곱/거듭제곱.java
+++ 백준/Silver/1740. 거듭제곱/거듭제곱.java
@@ -5,19 +5,24 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        long n = Long.parseLong(br.readLine());  
-        long result = 0;
-        long powerOfThree = 1;  
-
-        while (n > 0) {
-            if ((n & 1) == 1) {  
-                result += powerOfThree;
+        long n = Long.parseLong(br.readLine());
+        String bin = Long.toBinaryString(n);
+        String binary = new StringBuilder(bin).reverse().toString();
+        long answer = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            if (binary.charAt(i) == '1') {
+                if(i==0) {
+                    answer += 1;
+                } else {
+                    long sum = 1;
+                    for(int j=0; j<i; j++) {
+                        sum *=3;
+                    }
+                    answer += sum;
+                }
             }
-            powerOfThree *= 3;  // 다음 3의 거듭제곱
-            n >>= 1;            
         }
+        System.out.println(answer);
 
-        System.out.println(result);  
     }
 }
