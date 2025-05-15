944f35742699a0fa3b860fe51983cf2ef17e65a0
권채린 <92051742+chaelink@users.noreply.github.com>
2025-05-15 14:55:53 +0900

    [Gold IV] Title: 알파벳, Time: 912 ms, Memory: 15300 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,46 +1,46 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int r, c, maxLen = 0;
+    static int r,c,maxLen;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
     static char[][] arr;
-    static boolean[] visited = new boolean[26];  // 알파벳 A~Z 방문 여부
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
-
-    public static void dfs(int x, int y, int depth) {
-        int index = arr[x][y] - 'A';
-        visited[index] = true;
-        maxLen = Math.max(maxLen, depth);
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx >= 0 && nx < r && ny >= 0 && ny < c) {
-                int nextCharIndex = arr[nx][ny] - 'A';
-                if (!visited[nextCharIndex]) {
-                    dfs(nx, ny, depth + 1);
-                    visited[nextCharIndex] = false;  // 백트래킹
-                }
-            }
-        }
-    }
+    static boolean[] visited = new boolean[26];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] rc = br.readLine().split(" ");
-        r = Integer.parseInt(rc[0]);
-        c = Integer.parseInt(rc[1]);
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
         arr = new char[r][c];
-        for (int i = 0; i < r; i++) {
-            String line = br.readLine();
-            arr[i] = line.toCharArray();
-        }
 
-        dfs(0, 0, 1);
+        for (int i = 0; i < r; i++) {
+            String[] s = br.readLine().split("");
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = s[j].charAt(0);
+            }
+        }
+        dfs(0,0,1);
         System.out.println(maxLen);
+    }
+
+    public static void dfs(int i, int j, int depth) {
+        int alpaIdx = arr[i][j] - 'A';
+        visited[alpaIdx] = true;
+        maxLen = Math.max(maxLen, depth);
+
+        for (int k = 0; k < 4; k++) {
+            int nx = i + dx[k];
+            int ny = j + dy[k];
+
+            if(nx>=0 && nx<r && ny>=0 && ny<c) {
+                int nextAlpaIdx = arr[nx][ny] - 'A';
+                if(!visited[nextAlpaIdx]) {
+                    dfs(nx, ny, depth+1);
+                    visited[nextAlpaIdx] = false;
+                }
+            }
+        }
     }
 }
