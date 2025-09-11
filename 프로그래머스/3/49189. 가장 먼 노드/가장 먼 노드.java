b0af0fa8aadc004c0ccaca776647772f8c3a96ea
권채린 <92051742+chaelink@users.noreply.github.com>
2025-09-12 01:26:29 +0900

    [level 3] Title: 가장 먼 노드, Time: 34.45 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,47 +1,61 @@
 import java.util.*;
-      //가장 멀리 떨어진 노드의 갯수
-        //그래프를 돌면서 노드,거리 해시맵에 저장?
+
 class Solution {
+    
     public int solution(int n, int[][] edge) {
-        int[] visited = new int[n+1];
-        Queue<Integer> q = new ArrayDeque<>();
-        int answer = 0;
-        
-        List<List<Integer>> arr = new ArrayList<>();
-        for(int i=0; i<=n; i++) {
-            arr.add(i,new ArrayList<>());
+        //int edge2[][] = new int[n+1][n+1];
+        int m = edge.length;
+        ArrayList<ArrayList<Integer>> edge3 = new ArrayList<>();
+        for(int i=0; i<m; i++) {
+            edge3.add(new ArrayList<>());
         }
-        for(int i=0; i<edge.length; i++) {
+        
+        for(int i=0; i<m; i++) {
             int a = edge[i][0];
             int b = edge[i][1];
-            arr.get(a).add(b);
-            arr.get(b).add(a);
+            //edge2[a][b] = 1;
+            //edge2[b][a] = 1;
+            edge3.get(a).add(b);
+            edge3.get(b).add(a);
         }
+        
+        
+        int answer = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Queue<Integer> q = new ArrayDeque<>();
+        int visited[] = new int[n+1];
         
         q.add(1);
-        visited[1] = 0;
-        int maxLen = 0;
+        visited[1] = 1;
+        map.put(1,0);
+      
+        int maxn = 0;
         
         while(!q.isEmpty()) {
-            int now = q.remove();
-            int len = visited[now];
-            maxLen = Math.max(maxLen, len);
-            
-            for(int i : arr.get(now)) {
+            int now = q.poll();
+            for(int i : edge3.get(now)) {
                 if(visited[i]==0) {
                     q.add(i);
-                    visited[i] = len+1;
+                    visited[i] = 1;
+                    map.put(i, map.get(now)+1);
+                    maxn = Math.max(maxn, map.get(now)+1);
                 }
             }
+            // for(int i=1; i<=n; i++) {
+            //     //if(edge2[now][i]==1 && visited[i]==0) {
+            //         q.add(i);
+            //         visited[i] = 1;
+            //         map.put(i, map.get(now)+1);
+            //         maxn = Math.max(maxn, map.get(now)+1);
+            //     }
+            
         }
         
-        for(int i=2; i<=n; i++) {
-            System.out.println(visited[i]);
-            if(visited[i]==maxLen) {
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() == maxn) {
                 answer++;
             }
         }
-        
         return answer;
     }
 }
