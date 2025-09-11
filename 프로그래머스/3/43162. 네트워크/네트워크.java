e2157465df6008dd2bc63761940c1c39b6f93c98
권채린 <92051742+chaelink@users.noreply.github.com>
2025-09-12 00:55:41 +0900

    [level 3] Title: 네트워크, Time: 0.45 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,28 @@
 import java.util.*;
 
 class Solution {
+    int visited[];
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        int[] visited = new int[n];
+        visited = new int[n];
         Queue<Integer> q = new ArrayDeque<>();
         
         for(int i=0; i<n; i++) {
             if(visited[i]==0) {
-                visited[i] = 1;
                 q.add(i);
-                answer++;
+                
                 while(!q.isEmpty()) {
-                    int now = q.remove();
+                    int now = q.poll();
                     for(int j=0; j<n; j++) {
                         if(computers[now][j]==1 && visited[j]==0) {
                             q.add(j);
-                            visited[j] = 1;
+                            visited[j]=1;
                         }
                     }
                 }
+                answer++;
             }
         }
-        
         return answer;
     }
 }
