48db2ff898a20289809f4909caf94ff03f55fd4d
권채린 <92051742+chaelink@users.noreply.github.com>
2025-09-16 14:57:01 +0900

    [level 2] Title: 미로 탈출, Time: 16.80 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -2,78 +2,86 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] maps) {
-        int answer = -1; 
-        int sx=0; int sy=0; int li=0; int lj=0;int lt=0;
-        boolean lever = false;
+        boolean check = false;
+        int m1=0; int m2=0; int m3 =0;
+        int dx[] = {0,0,1,-1};
+        int dy[] = {1,-1,0,0};
+        int answer = -1;
+        int si=0;
+        int sj=0;
         int n = maps.length;
         int m = maps[0].length();
-        int[][] visited = new int[n][m];
-        int[] dx = {0,0,1,-1};
-        int[] dy = {1,-1,0,0};
         
-        List<List<Character>> map = new LinkedList<>();        
-        for(int i=0; i<maps.length; i++) {
-            map.add(i,new LinkedList<>());
-            String s = maps[i];
-            for(int j=0; j<s.length(); j++) {
-                map.get(i).add(s.charAt(j));
-                if(s.charAt(j)=='S') {
-                    sx = i;
-                    sy = j;
+        int[][] visited = new int[n][m];
+        List<List<Character>> map = new LinkedList<>();
+        
+        for(int i=0; i<n; i++) {
+            map.add(i, new LinkedList<>());  //1
+            for(int j=0; j<m; j++) {
+                map.get(i).add(maps[i].charAt(j));
+                if(maps[i].charAt(j)=='S') {
+                    si = i; 
+                    sj= j;
                 }
             }
         }
         
-        visited[sx][sy] = 1;
         Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{sx,sy,0});
-        
+        visited[si][sj] = 1;
+        q.add(new int[]{si, sj, 0});
+        //1. S -> L
         while(!q.isEmpty()) {
-            int[] now = q.remove();
-            int ni = now[0]; int nj = now[1]; int t = now[2];
-            if(map.get(ni).get(nj)=='L') {
-                lever = true;
-                li = ni;
-                lj = nj;
-                lt = t;
+            int[] now = q.poll();  //2
+            
+            int n1 = now[0]; int n2 = now[1]; int n3 = now[2];
+            if(map.get(n1).get(n2)=='L') {
+                check = true;
+                m1 = n1;
+                m2 = n2;
+                m3 = n3;
                 break;
             }
             
             for(int i=0; i<4; i++) {
-                int ii = ni + dx[i];
-                int jj = nj + dy[i];
-                int tt= t+1;
+                int ii = n1 + dx[i];
+                int jj = n2 + dy[i];
+                int time = n3+1;
                 
                 if(ii>=0 && ii<n && jj>=0 && jj<m && map.get(ii).get(jj)!='X' && visited[ii][jj]==0) {
-                    visited[ii][jj]=1;
-                    q.add(new int[]{ii,jj,tt});
+                    visited[ii][jj] = 1;
+                    q.add(new int[]{ii,jj,time});
                 }
-            }
-        }
-        if(!lever) {
-            return answer;
-        }
-        Queue<int[]> q2 = new ArrayDeque<>();
-        visited[li][lj] = 2;
-        q2.add(new int[]{li,lj,lt});
-        
-        while(!q2.isEmpty()) {
-            int[] now2 = q2.remove();
-            int ni2 = now2[0]; int nj2 = now2[1]; int t2 = now2[2];
-            if(map.get(ni2).get(nj2)=='E') {
-                return t2;
-            }
-            for(int i=0; i<4; i++) {
-                int ii2 = ni2 + dx[i];
-                int jj2 = nj2 + dy[i];
-                int tt2 = t2+1;
                 
-                if(ii2>=0 && ii2<n && jj2>=0 && jj2<m && map.get(ii2).get(jj2)!='X' && visited[ii2][jj2]!=2) {
-                    visited[ii2][jj2]=2;
-                    q2.add(new int[]{ii2,jj2,tt2});
-                }
             }
         }
+        if(!check) return answer;
+        
+        //앞에서 사용한 q 재사용 가능?
+        q.clear();
+        visited[m1][m2] = 2;
+        q.add(new int[]{m1,m2,m3});
+        
+        while(!q.isEmpty()) {
+            int[] now = q.poll();
+            if(map.get(now[0]).get(now[1])=='E') {
+                return now[2];
+            }
+            
+            for(int i=0; i<4; i++) {
+                int ii = now[0] + dx[i];
+                int jj = now[1] + dy[i];
+                int time = now[2] +1;
+                if(ii>=0 && ii<n && jj>=0 && jj<m && map.get(ii).get(jj)!='X' && visited[ii][jj]!=2) {
+                    visited[ii][jj] = 2;
+                    q.add(new int[]{ii,jj,time});
+                }
+            }
+            
+        }
+        
+        
+        //2. L -> E
+        
         return answer;
     }
 }
