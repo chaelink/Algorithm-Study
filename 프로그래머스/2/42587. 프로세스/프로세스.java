e661ea10141bca6fc544e263ed485d00d71a0911
권채린 <92051742+chaelink@users.noreply.github.com>
2025-11-18 22:45:57 +0900

    [level 2] Title: 프로세스, Time: 2.17 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,44 +3,44 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-       
-        Queue<Entry> q = new ArrayDeque<>();
-        int n = priorities.length;
         
-        for(int i=0; i<n; i++) {
-            q.add(new Entry(i,priorities[i]));
+        //현재 우선순위를 계속 체크
+        //해시셋으로 우선순위 중복 제거하고 취합 -> 배열 내림차순 정렬
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<priorities.length; i++) {
+            set.add(priorities[i]);
+        }
+        List<Integer> pro = new ArrayList<>();
+        // for(int i : set) {
+        //     pro.add(i);
+        // }
+        for(int i : priorities) {
+            pro.add(i);
+        }
+        Collections.sort(pro, Collections.reverseOrder());
+        int idx = 0;
+        
+        
+        //큐에서 빼서 출력 or 다시 넣기
+        Queue<int[]> q = new ArrayDeque<>();
+        for(int i=0; i<priorities.length; i++) {
+            q.add(new int[]{i, priorities[i]});
         }
         
-        int count = 0;
         while(!q.isEmpty()) {
-            Entry e = q.remove();
-            boolean up = false;
-            for(Entry en : q) {
-                if(en.p > e.p) up = true;
-            }
-            if(up) {
-                q.add(e);
-            } else {
-                if(e.idx == location) {
-                    count++;
-                    return count;
-                } else {
-                    count++;
+            int[] now = q.remove();
+            if(now[1] == pro.get(idx)) {
+                answer++;
+                idx++;
+                //인덱스가 로케이션이면 우선순위 리턴하고 끝
+                if(now[0] == location) {
+                    return answer;
                 }
+            } else {
+                q.add(now);
             }
-        }
-        return 0;
-
-    }
-    
-}
-
-class Entry {
-        int idx;
-        int p;
+        }  
         
-        public Entry(int idx, int p) {
-            this.idx = idx;
-            this.p = p;
-        }
+        return answer;
     }
+}
