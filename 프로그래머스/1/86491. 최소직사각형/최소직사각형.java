20be905fd139148d17b5f0e79a143af141926a3b
권채린 <92051742+chaelink@users.noreply.github.com>
2025-11-25 14:04:18 +0900

    [level 1] Title: 최소직사각형, Time: 1.47 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        //각 명함 별 가로 or 세로
+        //1만
+        //dp?
+        int n = sizes.length;
+        //int[][] dp = new int[n][n];
+        int a = 0;
+        int b = 0;
+        for(int i=0; i<sizes.length; i++) {
+            if(sizes[i][0] < sizes[i][1]) {
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+            a = Math.max(a, sizes[i][0]);
+            b = Math.max(b, sizes[i][1]);
+        }
+        
+        return a*b;
+    }
+}
