24b0a2d3bce088170e743bc8a96f9d6795de0245
권채린 <92051742+chaelink@users.noreply.github.com>
2025-11-26 23:36:53 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,19 +1,18 @@
+import java.util.*;
+
 class Solution {
     public long solution(int n) {
-        //long answer = 0;
+        long answer = 0;
         int[] dp = new int[n+1];
-        dp[0] = 1;
+        dp[0] = 0;
         dp[1] = 1;
-        //dp[2] = 2;
-        //dp[3] = 3;
-        //dp[4] = 5;
+        if(n>=2) {
+            dp[2] = 2;
+        }
+        for(int i=3; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
         
-        if(n==1) {
-            return 1;
-        }
-        for(int i=2; i<=n; i++) {
-            dp[i] = (dp[i-1]%1234567 + dp[i-2]%1234567)%1234567;
-        }
         return dp[n];
     }
 }
