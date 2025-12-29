2efce1399d210a9e05ba5c3a338e7c5278f2012d
권채린 <92051742+chaelink@users.noreply.github.com>
2025-12-30 01:37:47 +0900

    [level 1] Title: 예산, Time: 0.56 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/README.md
+++ 프로그래머스/1/12982. 예산/README.md
@@ -1,10 +1,10 @@
 # [level 1] 예산 - 12982 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982) 
 
 ### 성능 요약
 
-메모리: 73.8 MB, 시간: 0.94 ms
+메모리: 81.1 MB, 시간: 0.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 10일 14:02:55
+2025년 12월 30일 01:37:25
 
 ### 문제 설명
 
--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,19 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
+        Arrays.sort(d);
         
-        //1. d의 값들 pq에 넣기
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i=0; i<d.length; i++) {
-            pq.add(d[i]);
+            if(d[i]<=budget) {
+                answer++;
+                budget -= d[i];
+            } else {
+                break;
+            }
         }
-        
-        //2. pq의 값들을 하나씩 예산 배정
-        while(!pq.isEmpty() && budget >= pq.peek()) {
-            answer++;
-            budget -= pq.poll();
-        }
-        
         return answer;
     }
 }
