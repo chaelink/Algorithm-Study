8fb708217f475430097cffcb3f03ba60b6bbb36d
권채린 <92051742+chaelink@users.noreply.github.com>
2026-01-05 00:37:15 +0900

    [level 1] Title: 소수 만들기, Time: 18.31 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -2,32 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
+        //주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수
         int answer = 0;
         int n = nums.length;
-        int count = 0;
         int sum = 0;
         
         for(int i=0; i<n; i++) {
             for(int j=i+1; j<n; j++) {
                 for(int k=j+1; k<n; k++) {
-                    if(sosu(nums[i] + nums[j] + nums[k])) {
+                    sum = nums[i] + nums[j] + nums[k];
+                    if(sosu(sum)) {
                         answer++;
                     }
                 }
             }
         }
-        
+
         return answer;
     }
     
-//     public int dfs(int count, int sum, int[] nums) {
-//         if(count==3) return sum;
-//     }
-    
-    public boolean sosu(int a) {
-        if(a<=1) return false;
-        for(int b=2; b<a; b++) {
-            if(a%b==0) return false;
+    boolean sosu(int a) {
+        for(int i=2; i<a; i++) {
+            if(a%i==0) {
+                return false;
+            }
         }
         return true;
     }
