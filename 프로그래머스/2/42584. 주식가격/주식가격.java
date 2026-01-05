68908564e7007be44b5467f2a21ef22da7f27793
권채린 <92051742+chaelink@users.noreply.github.com>
2026-01-05 20:18:15 +0900

    [level 2] Title: 주식가격, Time: 70.45 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,32 +1,32 @@
-//문제도 짧고 만만하게 보았으나 큰코다침
-//한번에 안풀림 샤갈...
-
 import java.util.*;
+
 
 class Solution {
     public int[] solution(int[] prices) {
         int n = prices.length;
         int[] answer = new int[n];
-        Queue<Integer> q = new ArrayDeque<>();
         
+        //인덱스를 스택으로
+        Stack<Integer> st = new Stack<>();
+        
+        //처음부터 순회하면서 작으면 바로 결과 처리
         for(int i=0; i<n; i++) {
-            q.add(prices[i]);
-        }
-        int idx = 0;
-        while(!q.isEmpty()) {
-            int now = q.remove();
-            int time = 0;
-            for(int a : q) {
-                if(a >= now) time++;
-                else {
-                    time++; 
+            while(!st.isEmpty()) {
+                if(prices[st.peek()] > prices[i]) {
+                    int j = st.pop();
+                    answer[j] = i-j;
+                } else {
                     break;
                 }
             }
-            answer[idx] = time;
-            idx++;
+            st.push(i);
         }
         
+        //끝까지 다 순회한 후 남은 값 처리
+        while(!st.isEmpty()) {
+            int index = st.pop();
+            answer[index] = n-1 - index;
+        }
         return answer;
     }
 }
