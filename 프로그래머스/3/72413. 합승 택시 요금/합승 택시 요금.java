ac1c8eee0b9f86dd48db0c2eb97de19e2a570ce8
권채린 <92051742+chaelink@users.noreply.github.com>
2026-01-07 09:09:12 +0900

    [level 3] Title: 합승 택시 요금, Time: 25.89 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -3,13 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int n, int s, int a, int b, int[][] fares) {
         int answer = 0;
-				//✅ 인풋을 본인이 쓰기 편한 구조로 바꾸기(다익스트라) => 무방향 그래프 만들기
+		//✅ 인풋을 본인이 쓰기 편한 구조로 바꾸기(다익스트라) => 무방향 그래프 만들기
         int[][] costs = new int[n+1][n+1];
         for (int[] fare : fares) {
             costs[fare[0]][fare[1]] = fare[2];
             costs[fare[1]][fare[0]] = fare[2];
 		    }
-				//✅ s, a, b 3개의 노드에서 모든 노드까지 도달하는 최소 비용을 저장한다.
+		//✅ s, a, b 3개의 노드에서 모든 노드까지 도달하는 최소 비용을 저장한다.
         int[][] distArray = new int[3][n+1];
         for (int[] d : distArray) {
         	Arrays.fill(d, Integer.MAX_VALUE);
@@ -34,7 +34,7 @@ class Solution {
                 }
             }
         }
-				//✅ 모든 노드를 순회하며 cost(s->x) + cost(x->a) + cost(x->b)의 최소비용을 반환한다.
+		//✅ 모든 노드를 순회하며 cost(s->x) + cost(x->a) + cost(x->b)의 최소비용을 반환한다.
         int minCost = Integer.MAX_VALUE;
         for (int i = 1; i <= n; i++) {
             int sum = 0;
