e93ebde2239cf8950eae6936b25b71b1f46d7633
권채린 <92051742+chaelink@users.noreply.github.com>
2026-02-21 17:39:42 +0900

    [level 2] Title: 기능개발, Time: 0.17 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 93.1 MB, 시간: 0.11 ms
+메모리: 86.5 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 05일 21:01:49
+2026년 02월 21일 17:39:37
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,51 +1,39 @@
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int[] progresses, int[] speeds) {
-        List<Integer> answer = new ArrayList<>();
+    public int[] solution(int[] progresses, int[] speeds) {
         
-        //현재 진도, 개발 속도
-        int n = speeds.length;
-        int idx = 0;
-        while(idx < n) {
-            if(progresses[idx]>=100) {
-                int count = 0;
-                for(int i=idx; i<n; i++) {
-                    if(progresses[i]>=100) {count++;}
-                    else {
-                        break;}
-                }
-                answer.add(count);
-                idx += count;
-            } else {
-                while(progresses[idx]<100) {
-                    for(int i=idx; i<n; i++) {
-                        progresses[i] += speeds[i];
-                    }
+        List<Integer> list = new ArrayList<>();
+        int n = progresses.length;
+        //각 기능의 개발속도는 다름
+        //100개 이하
+        //
+        int i = 0;
+        while(i<n) {
+            int temp = 0;
+            for(int i2=i; i2<n; i2++) {
+                progresses[i2] += speeds[i2];
+            }
+            for(int i2=i; i2<n; i2++) {
+                if(progresses[i2]>=100) {
+                    temp++;
+                } else {
+                    break;
                 }
             }
+            if(temp>0) {
+                list.add(temp);
+                i += temp;
+            }
         }
-       
-//         while (idx < n) {
-//             // 하루 단위 진도 증가
-//             for (int i = idx; i < n; i++) {
-//                 progresses[i] += speeds[i];
-//             }
-
-//             // 연속된 배포 가능한 기능 세기 
-//             int count = 0;
-//             for (int i = idx; i < n; i++) {
-//                 if (progresses[i] >= 100) { //idx==현재 가장 앞 기능의 인덱스
-//                     count++;
-//                 } else {
-//                     break; // 앞에서부터 연속된 것만 체크해야 하므로 바로 break
-//                 }
-//             }
-
-//             if (count > 0) {
-//                 answer.add(count);
-//                 idx += count; //완료된 만큼 인덱스 증가           }
-//         }
+        
+        int[] answer = new int[list.size()];
+        int a = 0;
+        for(int num :list) {
+            answer[a] = num;
+            a++;
+        }
+        
         
         return answer;
     }
