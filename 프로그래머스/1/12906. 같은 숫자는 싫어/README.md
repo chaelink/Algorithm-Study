10a2202042ed649ebb05378d575fb293ad756044
권채린 <92051742+chaelink@users.noreply.github.com>
2026-03-08 20:06:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.78 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 111 MB, 시간: 62.11 ms
+메모리: 111 MB, 시간: 39.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 21일 22:57:26
+2026년 03월 08일 20:06:05
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,28 +2,27 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
+      
+        //arr는 0부터 9
+        //백만개 이하
         
-        List<Integer> ans = new ArrayList<>();
-        Stack<Integer> st = new Stack<>();
+        //다르면 결과 집합에 추가
         
-        st.push(arr[0]);
+        Queue<Integer> q = new ArrayDeque<>();
+        int num = arr[0];
+        q.add(num);
         for(int i=1; i<arr.length; i++) {
-            if(st.peek() != arr[i]) {
-                ans.add(st.pop());
-                st.push(arr[i]);
+            if(num==arr[i]) {
+                
             } else {
-                continue;
+                num = arr[i];
+                q.add(num);
             }
         }
         
-        if(!st.isEmpty()) {
-            ans.add(st.pop());
-        }
-        
-        int[] answer = new int[ans.size()];
-        int idx = 0;
-        for(Integer i : ans) {
-            answer[idx] = i; idx++;
+        int[] answer = new int[q.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = q.remove();
         }
         
         return answer;
