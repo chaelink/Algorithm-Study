14bb31d62c1efda04976b9d992f2a8997fd5cb4f
권채린 <92051742+chaelink@users.noreply.github.com>
2026-03-17 23:17:15 +0900

    [level 2] Title: 주식가격, Time: 37.71 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,20 +1,15 @@
 import java.util.*;
 
-
 class Solution {
     public int[] solution(int[] prices) {
         int n = prices.length;
         int[] answer = new int[n];
-        
-        //인덱스를 스택으로
         Stack<Integer> st = new Stack<>();
-        
-        //처음부터 순회하면서 작으면 바로 결과 처리
         for(int i=0; i<n; i++) {
             while(!st.isEmpty()) {
-                if(prices[st.peek()] > prices[i]) {
-                    int j = st.pop();
-                    answer[j] = i-j;
+                if(prices[st.peek()]>prices[i]) {
+                    int idx = st.pop();
+                    answer[idx] = i - idx;
                 } else {
                     break;
                 }
@@ -22,10 +17,9 @@ class Solution {
             st.push(i);
         }
         
-        //끝까지 다 순회한 후 남은 값 처리
         while(!st.isEmpty()) {
-            int index = st.pop();
-            answer[index] = n-1 - index;
+            int idx = st.pop();
+            answer[idx] = n -1 - idx; 
         }
         return answer;
     }
