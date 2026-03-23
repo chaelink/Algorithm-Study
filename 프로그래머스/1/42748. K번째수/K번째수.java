6117ef60a3564e6d7d55b9f0fa79980a37148da6
권채린 <92051742+chaelink@users.noreply.github.com>
2026-03-23 23:28:54 +0900

    [level 1] Title: K번째수, Time: 0.46 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,22 +4,20 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int n = commands.length;
         int[] answer = new int[n];
+        int m = 0;
         
-        for(int i=0; i<n; i++) {
-            int size = commands[i][1] - commands[i][0] +1;
-            int[] arr = new int[size];
-            
-            int idx = 0;
-            //2 부터 5
-            for(int j=commands[i][0]; j<=commands[i][1]; j++) {
-                arr[idx] = array[j-1];
-                //System.out.println(arr[idx]);
+        for(int[] a : commands) {
+            int[] temp = new int[a[1] - a[0]+1];
+            int idx =0;
+            for(int i=a[0]; i<=a[1]; i++) { 
+                temp[idx] = array[i-1];
                 idx++;
             }
-            
-            Arrays.sort(arr);
-            answer[i] = arr[commands[i][2]-1];
+            Arrays.sort(temp);
+            answer[m] = temp[a[2] -1];
+            m++;
         }
+        
         return answer;
     }
 }
