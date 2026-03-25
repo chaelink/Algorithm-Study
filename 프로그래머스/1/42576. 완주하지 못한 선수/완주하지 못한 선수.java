d09d0c9383d13de0bace5bce3807582ca553ca1a
권채린 <92051742+chaelink@users.noreply.github.com>
2026-03-25 21:25:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 102.50 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,22 +4,17 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         HashMap<String, Integer> map = new HashMap<>();
-        
-        for(String str : participant) {
-            if(map.containsKey(str)) {
-                map.put(str, map.get(str)+1);
-            } else {
-                map.put(str,1);
-            }
+        for(String name : participant) {
+            map.put(name, map.getOrDefault(name,0)+1);
         }
         
-        for(String str : completion) {
-            map.put(str, map.get(str)-1);
+        for(String name : completion) {
+            map.put(name, map.get(name)-1);
         }
         
-        for(String name : map.keySet()) {
-            if(map.get(name)!=0) {
-                answer = name;
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            if(entry.getValue()==1) {
+                answer = entry.getKey();
             }
         }
         
