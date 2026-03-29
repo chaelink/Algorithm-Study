79bb0d4b01a0e34b28190570f0a725b202ed4d57
권채린 <92051742+chaelink@users.noreply.github.com>
2026-03-29 17:13:01 +0900

    [level 2] Title: 가장 큰 수, Time: 194.32 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,26 +2,24 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        StringBuilder answer = new StringBuilder();
+        StringBuilder answer = new StringBuilder("");
         int n = numbers.length;
-
-        List<String> str = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            str.add(String.valueOf(numbers[i]));
+        
+        String[] str = new String[n];
+        for(int i=0; i<n; i++) {
+            str[i] = numbers[i]+"";
         }
-
-        // 핵심: (b+a)와 (a+b)를 비교
-        Collections.sort(str, (a, b) -> (b + a).compareTo(a + b));
-
-        // 예외 처리: [0, 0, 0] 같은 경우 "000"이 아니라 "0"
-        if (str.get(0).equals("0")) {
+        
+        Arrays.sort(str, (a,b) -> (b+a).compareTo(a+b));
+        
+        if(str[0].equals("0")) {
             return "0";
         }
-
-        for (String s : str) {
-            answer.append(s);
+        
+        for(int i=0; i<n; i++) {
+            answer.append(str[i]);
         }
-
+        
         return answer.toString();
     }
-}
+} 
