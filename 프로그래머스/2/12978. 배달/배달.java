3f77869b57442585e6aeee75f1d98f4542feaeac
권채린 <92051742+chaelink@users.noreply.github.com>
2026-03-31 08:42:30 +0900

    [level 2] Title: 배달, Time: 3.66 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,59 +1,45 @@
 import java.util.*;
-
 class Solution {
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        
-        //그래프 저장
-        List<List<Edge>> list = new ArrayList<>();
-        for(int i=0; i<=N; i++) {
-            list.add(new ArrayList<>());
+
+        List<int[]>[] graph = new ArrayList[N+1];
+        for(int i=1; i<=N; i++) graph[i] = new ArrayList<>();
+        for(int[] a : road) {
+            graph[a[0]].add(new int[]{a[1], a[2]});
+            graph[a[1]].add(new int[]{a[0], a[2]});
         }
-        for(int i=0; i<road.length; i++) {
-            int a = road[i][0]; int b = road[i][1]; int c = road[i][2];
-            list.get(a).add(new Edge(b,c));
-            list.get(b).add(new Edge(a,c));
-        }
-        
-        //최종 시간 기록
-        int[] time = new int[N+1];
-        Arrays.fill(time, Integer.MAX_VALUE);
-        
-        Queue<int[]> q = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        
-        //1번 마을에서 출발
-        time[1] = 0;
-        q.add(new int[]{1,0});
-        
-        //pq반복
-        while(!q.isEmpty()) {
-            int[] now = q.remove();
-            
-            if(time[now[0]] < now[1]) continue;
-            
-            for(Edge e : list.get(now[0])) {
-                int nd = now[1] + e.to;
-                if(nd < time[e.node]) {
-                    time[e.node] = nd;
-                    q.add(new int[]{e.node, nd});
+
+        int[] result = new int[N+1];
+        Arrays.fill(result, Integer.MAX_VALUE);
+
+        // {비용, 마을번호} 비용 기준 오름차순
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]);
+        pq.add(new int[]{0, 1});
+        result[1] = 0;
+
+        while(!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int cost = curr[0];
+            int idx = curr[1];
+
+            if(result[idx] < cost) continue; // 이미 더 싸게 방문
+
+            for(int[] next : graph[idx]) {
+                int nextIdx = next[0];
+                int nextCost = cost + next[1];
+
+                if(result[nextIdx] > nextCost) {
+                    result[nextIdx] = nextCost;
+                    pq.add(new int[]{nextCost, nextIdx});
                 }
             }
-            
         }
+
         for(int i=1; i<=N; i++) {
-            if(time[i]<=K) answer++;
+            if(result[i] <= K) answer++;
         }
 
         return answer;
-    }
-}
-
-class Edge {
-    public int node;
-    public int to;
-    
-    public Edge(int node, int to) {
-        this.node = node;
-        this.to = to;
     }
 }
