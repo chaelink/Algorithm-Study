39fd5b4a1e77794efe813deae779b487a40769cc
권채린 <92051742+chaelink@users.noreply.github.com>
2026-04-04 13:17:51 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
+        
         answer[0] = gcd(n,m);
-        answer[1] = n*m/ answer[0];
+        answer[1] = lcm(n,m);
         
         return answer;
     }
     
-    public int gcd(int a, int b) {
-        int r;
-        while(b >0) {
-            r = a % b;
-            a = b;
-            b = r;
-        }
-        return a;
+    int gcd(int a, int b) {
+        if(b==0) return a;
+        return gcd(b, a%b);
+    }
+    
+    int lcm(int a, int b) {
+        return a/gcd(a,b)*b;
     }
 }
