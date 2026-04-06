72d68d9c8beb130fcc79858c83912233810c5232
권채린 <92051742+chaelink@users.noreply.github.com>
2026-04-06 23:04:37 +0900

    [level 2] Title: H-Index, Time: 21.46 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,24 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        Arrays.sort(citations);
         int n = citations.length;
-        int h = citations[n-1];
-        //System.out.println(h);
+        Arrays.sort(citations);
+        int max = citations[n-1];
         
-        for(int i=h; i>=0; i--) {
-            int l = 0;
-            int u = 0;
+        for(int i=max; i>=0; i--) {
+            int up=0; int down = 0;
             for(int j=0; j<n; j++) {
-                if(citations[j]>=i) {
-                    u++;
-                } else {
-                    l++;
-                }
+                if(citations[j]>=i) up++;
+                else down++;
             }
-            //System.out.println(u);
-            //System.out.println(l);
-            if(u>=i && l<=i) return i;
+            if(up>=i) return i;
         }
         
         return answer;
