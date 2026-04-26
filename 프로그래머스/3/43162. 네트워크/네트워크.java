9ff5407bb630c5829decb91adc13a7c0fcaf5408
권채린 <92051742+chaelink@users.noreply.github.com>
2026-04-26 13:14:02 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Solution {
     int[] visited;
     public int solution(int n, int[][] computers) {
@@ -8,20 +7,19 @@ class Solution {
         
         for(int i=0; i<n; i++) {
             if(visited[i]==0) {
-                visited[i]=1;
-                dfs(computers, i);
                 answer++;
+                dfs(i, computers);
             }
         }
+        
         return answer;
     }
     
-    void dfs(int[][] computers, int k) {
-        int n = computers.length;
-        for(int i=0; i<n; i++) {
-            if(computers[k][i]==1 && visited[i]==0) {
-                visited[i]=1;
-                dfs(computers,i);
+    void dfs(int idx, int[][] computers) {
+        for(int i=0; i<computers.length; i++) {
+            if(computers[i][idx]==1 && visited[i]==0) {
+                visited[i] = 1;
+                dfs(i,computers);
             }
         }
     }
