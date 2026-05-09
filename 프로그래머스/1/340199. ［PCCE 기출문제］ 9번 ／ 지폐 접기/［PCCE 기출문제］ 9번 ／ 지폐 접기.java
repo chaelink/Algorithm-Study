dfa1b68bab7cf64a04b0e08533f9b4a73e3fd602
권채린 <92051742+chaelink@users.noreply.github.com>
2026-05-09 21:30:39 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.31 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+        //항상 길이가 긴 쪽을 반으로 접음
+        Arrays.sort(wallet);
+        Arrays.sort(bill);
+        
+        while(wallet[0] < bill[0] || wallet[1] < bill[1]) {
+            bill[1] = bill[1]/2;
+            Arrays.sort(bill);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
