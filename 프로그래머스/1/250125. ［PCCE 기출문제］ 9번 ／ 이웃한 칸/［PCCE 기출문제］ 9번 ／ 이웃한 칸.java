90577b187f76b3861f140a96a84242da533ee665
권채린 <92051742+chaelink@users.noreply.github.com>
2026-05-12 23:39:08 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.02 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] board, int h, int w) {
+        int answer = 0;
+        int n = board.length;
+        int m = board[0].length;
+        int[] dx = {0,0,1,-1};
+        int[] dy = {1,-1,0,0};
+        String color = board[h][w];
+        
+        for(int i=0; i<4; i++) {
+            int nx = h + dx[i];
+            int ny = w + dy[i];
+            
+            if(nx>=0 && nx<n && ny>=0 && ny<m) {
+                if(board[nx][ny].equals(color)) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
