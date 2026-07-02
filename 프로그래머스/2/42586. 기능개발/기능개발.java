1022cd43a08f96f3c593d0204e7524ef97363096
권채린 <92051742+chaelink@users.noreply.github.com>
2026-07-02 22:57:41 +0900

    [level 2] Title: 기능개발, Time: 0.47 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,32 +1,40 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        
-        List<Integer> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        int n = progresses.length;
         int idx = 0;
-        int n = speeds.length;
-        while(idx < n) {
-            if(progresses[idx]>=100) {
-                Deque<Integer> q = new ArrayDeque<>();
-                for(int i=idx; i<n; i++) {
-                    if(progresses[i]>=100) q.add(i);
-                    else break;
-                }
-                ans.add(q.size());
-                idx += q.size();
+        Stack<Integer> st = new Stack<>();
+        
+        while(idx<n) {
+            if(!st.isEmpty()) {
+                list.add(st.size());
+                idx += st.size();
+                st = new Stack<>();
             }
+            if(idx==n) break;
             
             for(int i=idx; i<n; i++) {
                 progresses[i] += speeds[i];
             }
+            
+            if(progresses[idx]>=100) {
+                for(int i=idx; i<n; i++) {
+                    if(progresses[i]>=100) {
+                        st.push(i);
+                    } else {break;}
+                }
+            }
         }
         
-        int[] answer = new int[ans.size()];
-        for(int i=0; i<ans.size(); i++) {
-            answer[i] = ans.get(i);
+        int[] answer = new int[list.size()];
+        int i=0;
+        for(Integer in : list) {
+            answer[i] = in;
+            i++;
         }
-        
-        
+            
         return answer;
     }
 }
