8d3764cf985825f49e726e9a964b2f528f1eedb4
권채린 <92051742+chaelink@users.noreply.github.com>
2026-07-31 11:53:41 +0900

    [level 2] Title: 피로도, Time: 1.83 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,28 +1,34 @@
 import java.util.*;
-
 class Solution {
-    int count = 0;
-    int n;
-    int[] visited;
-    
+    int answer = 0;
+    int[] visit;
     public int solution(int k, int[][] dungeons) {
-        n = dungeons.length;
-        visited = new int[n];
         
-        dfs(dungeons, k, 0);
-        return count;
+        //던전이 8개 이하다 -> 2의n제곱 시간 복잡도인 완전탐색이 가능하다
+        visit = new int[dungeons.length];
+        for(int i=0; i<dungeons.length; i++) {
+            if(k>=dungeons[i][0]) {
+                visit[i]=1;
+                k -= dungeons[i][1];
+                dfs(k,1,dungeons);
+                k += dungeons[i][1];
+                visit[i] = 0; 
+            }
+        }
+        
+        return answer;
     }
     
-    public void dfs(int[][] dungeons, int k, int c) {
-        count = Math.max(count, c);
+    void dfs(int k, int n, int[][] dungeons) {
+        answer = Math.max(answer, n);
         
-        for(int i=0; i<n; i++) {
-            if(visited[i]==0 && k>= dungeons[i][0]) {
-                visited[i] = 1;
+        for(int i=0; i<dungeons.length; i++) {
+            if(visit[i]==0 && k>=dungeons[i][0]) {
+                visit[i]=1;
                 k -= dungeons[i][1];
-                dfs(dungeons, k, c+1);
+                dfs(k,n+1, dungeons);
                 k += dungeons[i][1];
-                visited[i] = 0;
+                visit[i] = 0;
             }
         }
     }
