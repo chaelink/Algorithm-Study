e010746fb01f7d2fa5bf635953709c91498498f8
권채린 <92051742+chaelink@users.noreply.github.com>
2026-07-31 16:25:16 +0900

    [level 2] Title: 타겟 넘버, Time: 3.27 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,26 +1,21 @@
 import java.util.*;
-
 class Solution {
     int answer = 0;
-    int n = 0;
     public int solution(int[] numbers, int target) {
-        n = numbers.length;
+        //주어진 숫자가 20개 이하이므로 완전탐색 가능
         
-        dfs(0, 0, numbers, target);
+        dfs(0,0,numbers, target);
         
         return answer;
     }
     
-    void dfs(int idx, int num, int[] numbers, int target) {
-        if(idx==n) {
-            if(num == target) {
-                answer++;
-            }
+    void dfs(int depth, int num, int[] numbers, int target) {
+        if(depth == numbers.length) {
+            if(num==target) answer++;
             return;
         }
         
-        dfs(idx+1, num + numbers[idx], numbers, target);
-        dfs(idx+1, num - numbers[idx], numbers, target);
-        
+        dfs(depth+1, num + numbers[depth], numbers, target);
+        dfs(depth+1, num - numbers[depth], numbers, target);       
     }
 }
