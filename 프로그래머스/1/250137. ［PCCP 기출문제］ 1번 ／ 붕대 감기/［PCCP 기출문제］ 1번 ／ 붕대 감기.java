e3f57661826c3ba25a3c259bc1ecc37989651937
권채린 <92051742+chaelink@users.noreply.github.com>
2026-08-01 12:40:48 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.04 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -6,27 +6,43 @@ class Solution {
         int succ = 0;
         int hp = health;
         int idx = 0;
+        int time = 0;
         
-        for(int i=1; i<=last; i++) {
-            
-            //몬스터 공격 여부 확인
-            if(i == attacks[idx][0]) {
-                hp -= attacks[idx][1];
-                if(hp<=0) return -1;
-                idx++;
-                succ = 0;
-                continue;
+        for(int i=0; i<attacks.length; i++) {
+            int diff = attacks[i][0] - time-1;
+            for(int j=0; j<diff; j++) {
+                hp+= bandage[1];
             }
-            
-            //몬스터 공격 받지 않는 경우
-            succ++;
-            hp += bandage[1];
-            if(succ==bandage[0]) {
-                hp += bandage[2];
-                succ=0;
+            if(diff/bandage[0]>0) {
+                hp += (diff/bandage[0])*bandage[2];
             }
-            hp = Math.min(hp,health);
+            if(hp>health) hp = health;
+            
+            hp -= attacks[i][1];
+            if(hp<=0) return -1;
+            time = attacks[i][0];
+            //System.out.println(hp);
         }
+        
+//         for(int i=1; i<=last; i++) {     
+//             //몬스터 공격 여부 확인
+//             if(i == attacks[idx][0]) {
+//                 hp -= attacks[idx][1];
+//                 if(hp<=0) return -1;
+//                 idx++;
+//                 succ = 0;
+//                 continue;
+//             }
+            
+//             //몬스터 공격 받지 않는 경우
+//             succ++;
+//             hp += bandage[1];
+//             if(succ==bandage[0]) {
+//                 hp += bandage[2];
+//                 succ=0;
+//             }
+//             hp = Math.min(hp,health);
+//         }
         
         return hp;
         
