f92bca3e50f83ff49bf90f435b1c8d3573391e6c
권채린 <92051742+chaelink@users.noreply.github.com>
2026-08-12 16:14:46 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.37 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,36 +1,38 @@
 import java.util.*;
 
-//최단거리는 bfs
 class Solution {
-    int[] dx = {0,0,1,-1};
-    int[] dy = {1,-1,0,0};
     public int solution(int[][] maps) {
-        int n =  maps.length;
-        int m = maps[0].length;
-        //int answer = 0;
-        int[][] visited = new int[n][m];
-        visited[0][0] = 1;
         
-        Queue<Integer[]> q = new LinkedList<>();
-        q.add(new Integer[]{0,0,0});
+        //가장 빠른 방법 -> bfs 사용
+        int n = maps.length;
+        int m = maps[0].length;
+        int[] dx = {0,0,1,-1};
+        int[] dy = {1,-1,0,0};
+        
+        int[][] arr = new int[n][m];
+        
+        Queue<int[]> q = new ArrayDeque<>();
+        arr[0][0] = 1;
+        
+        q.add(new int[]{0,0});
         
         while(!q.isEmpty()) {
-            Integer[] now = q.remove();
-            if(now[0]==n-1 && now[1]==m-1) {
-                return now[2]+1;
-            }
+            int[] now = q.remove();
+            
             for(int i=0; i<4; i++) {
                 int nx = now[0] + dx[i];
                 int ny = now[1] + dy[i];
-                if(nx>=0 && nx<n && ny>=0 && ny<m) {
-                    if(visited[nx][ny]==0 && maps[nx][ny]==1) {
-                        visited[nx][ny] = 1;
-                        q.add(new Integer[]{nx,ny,now[2]+1});
-                    }
+                if(nx>=0 && nx<n && ny>=0 && ny<m && maps[nx][ny]==1 && arr[nx][ny]==0) {
+                    arr[nx][ny] = arr[now[0]][now[1]] +1;
+                    q.add(new int[]{nx,ny});
                 }
             }
         }
         
-        return -1;
+        if(arr[n-1][m-1]==0) {
+            return -1;
+        } else {
+            return arr[n-1][m-1];
+        }
     }
 }
