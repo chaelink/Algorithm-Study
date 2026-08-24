315d1ef68f9d861120dd5fc8f4db8ef65da9e048
권채린 <92051742+chaelink@users.noreply.github.com>
2026-08-24 19:41:38 +0900

    [level 2] Title: 호텔 대실, Time: 6.65 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,33 +1,35 @@
 import java.util.*;
-//35 ~
+
 class Solution {
     public int solution(String[][] book_time) {
-        int answer = 0;
-        List<Integer> list = new ArrayList<>();
-        Arrays.sort(book_time, (a,b)-> {
+        int answer = 1;
+        
+        //입실 순서대로 정렬
+        Arrays.sort(book_time, (a,b) -> {
             return a[0].compareTo(b[0]);
         });
-        for(String[] str : book_time) {
-            String[] in = str[0].split(":");
-            int in_time = Integer.valueOf(in[0])*60 + Integer.valueOf(in[1]);
-            String[] out = str[1].split(":");
-            int out_time = Integer.valueOf(out[0])*60 + Integer.valueOf(out[1]);
-            boolean done = false;
-            for(Integer room : list) {
-                if(room<=in_time) {
-                    list.remove(room);
-                    list.add(out_time+10);
-                    done = true;
-                    break;
-                }
-            }
+        
+        PriorityQueue<Integer> list = new PriorityQueue<>();
+        list.add( change(book_time[0][1])+10 );
+        
+        for(int i=1; i<book_time.length; i++) {
+            String now = book_time[i][0];
+            int time = change(now);
             
-            if(!done) {
-                list.add(out_time+10);
+            if(list.size()>0 && list.peek()<=time) {
+                list.remove();
+            } else {
                 answer++;
             }
+             
+            list.add(change(book_time[i][1])+10);           
         }
-        
+           
         return answer;
+    }
+    
+    int change(String str) {
+        String[] s = str.split(":");
+        return Integer.parseInt(s[0])*60 + Integer.parseInt(s[1]);
     }
 }
