717c416f32b55d09fe66e54ab3a049cde0c95b72
권채린 <92051742+chaelink@users.noreply.github.com>
2026-08-26 14:54:41 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 4.12 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 19.15 ms
+메모리: 87.6 MB, 시간: 4.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 12일 13:35:40
+2026년 08월 26일 14:54:36
 
 ### 문제 설명
 
--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,23 +1,26 @@
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[2];
-        int time=0; int zero =0;
         
+        int count =0;
+        int removed = 0;
         
-        while(!s.equals("1")) {
-            int o = s.length();
-            s = s.replace("0","");
-            int l = s.length();
-            zero += (o-l);
-            s="";
-            while(l!=0) {
-                s += l % 2;
-                l = l/2;
+        while(s.length()>1) {
+            //순회, 0 갯수, 1갯수 카운트
+            int length = 0;
+            for(int i=0; i< s.length(); i++) {
+                if(s.charAt(i)=='0') removed++;
+                else length++;
             }
-            time++;
+            
+            //이진 변환해서 s에 저장
+            s = Integer.toString(length, 2);
+            count++;
         }
-        answer[0] = time;
-        answer[1] = zero;
+        
+        answer[0] = count;
+        answer[1] = removed;
+        
         return answer;
     }
 }
