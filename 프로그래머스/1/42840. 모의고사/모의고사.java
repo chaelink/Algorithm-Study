29fe1ba6e0ef22048560f20824b7cd1cf57090ee
권채린 <92051742+chaelink@users.noreply.github.com>
2026-08-27 17:14:00 +0900

    [level 1] Title: 모의고사, Time: 0.53 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,46 +1,39 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
-        int[] su1 = new int[]{1,2,3,4,5};
-        int[] su2 = new int[]{2,1,2,3,2,4,2,5};
-        int[] su3 = new int[]{3,3,1,1,2,2,4,4,5,5};
+        int[] answer = new int[3];
+        // 1번 : 12345 
+        //2번 : 
+        //3번 : 
+        int[] n1 = {1,2,3,4,5};
+        int[] n2 = {2,1,2,3,2,4,2,5};
+        int[] n3 = {3,3,1,1,2,2,4,4,5,5};
         
-        int a=0,b=0,c=0;
-        //1만 문제
         for(int i=0; i<answers.length; i++) {
-            if(answers[i] == su1[i%5]) a++;
-            if(answers[i] == su2[i%8]) b++;
-            if(answers[i] == su3[i%10]) c++;
+            int n = answers[i];
+            if(n == n1[i%5]) answer[0]++;
+            if(n == n2[i%8]) answer[1]++;
+            if(n == n3[i%10]) answer[2]++;
+        }
+        int max = Math.max(Math.max(answer[0],answer[1]),answer[2]);
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<3; i++) {
+            if(answer[i] == max) list.add(i+1);
+            //System.out.println(answer[i]);
         }
         
-        int max = Math.max(Math.max(a,b),c);
-        int n=0;
         
-        int idx = 0;
-        if(max == a) {
-            n++;
-        }
-        if(max == b) {
-            n++;
-        }
-        if(max == c) {
-            n++;   
-        }
-        int[] answer = new int[n];
-        if(max == a) {
-            answer[idx] = 1;
+        
+        int size = list.size();
+        int[] ans = new int[size];
+        int idx=0;
+        for(Integer number : list) {
+            ans[idx] = number;
             idx++;
         }
-        if(max == b) {
-            answer[idx] = 2;
-            idx++;
-        }
-        if(max == c) {
-            answer[idx] = 3;
-            idx++;  
-        }
-        Arrays.sort(answer);
-        return answer;
+        
+        return ans;
     }
 }
