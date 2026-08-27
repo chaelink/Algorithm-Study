085e8b7f83a969b0c0ba8c288d3815b731773938
권채린 <92051742+chaelink@users.noreply.github.com>
2026-08-27 20:38:07 +0900

    [level 2] Title: 소수 찾기, Time: 66.15 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,43 +1,43 @@
 import java.util.*;
 class Solution {
-    //최종 소수를 저장할 셋
+    //소수
     Set<Integer> set = new HashSet<>();
     int[] visit;
-    int n;
-    String[] arr;
     public int solution(String numbers) {
         
-        n = numbers.length();
-        visit = new int[n];
-        arr = numbers.split("");
-        
         StringBuilder sb = new StringBuilder("");
-        dfs(sb);
+        String[] number = numbers.split("");
+        visit = new int[numbers.length()];
+        
+        dfs(sb, number);
         
         return set.size();
     }
     
-    void dfs(StringBuilder sb) {
+    void dfs(StringBuilder sb, String[] number) {
         if(sb.length()>0) {
             int num = Integer.parseInt(sb.toString());
-            if(sosu(num)) set.add(num);
+            if(sosu(num)) {
+                set.add(num);
+            }
         }
         
-        for(int i=0; i<n; i++) {
+        
+        for(int i=0; i<number.length; i++) {
             if(visit[i]==0) {
-                visit[i] = 1;
-                sb.append(arr[i]);
-                dfs(sb);
+                visit[i]=1;
+                dfs(sb.append(number[i]), number);
                 sb.deleteCharAt(sb.length()-1);
-                visit[i] = 0;
+                visit[i]=0;
             }
         }
     }
     
-    boolean sosu(int n) {
-        if(n<=1) return false;
-        for(int i=2; i*i<=n; i++) {
-            if(n%i==0) return false;
+    boolean sosu(int num) {
+        if(num<=1) return false;
+        if(num==2) return true;
+        for(int i=2; i<num; i++) {
+            if(num%i==0) return false;
         }
         return true;
     }
