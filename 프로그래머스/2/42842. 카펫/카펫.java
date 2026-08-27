fdb1190db22cb37e2219d59b2e40e05721745131
권채린 <92051742+chaelink@users.noreply.github.com>
2026-08-27 22:15:03 +0900

    [level 2] Title: 카펫, Time: 3.15 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,40 +1,27 @@
 import java.util.*;
 class Solution {
-    TreeSet<Integer> list = new TreeSet<>();
-    int[] answer = new int[2];
-    //List<Integer> ans = new ArrayList<>();
-    int b;
     public int[] solution(int brown, int yellow) {
-        b = brown;
-        int row, col;
-        int sum = brown + yellow;
+        int[] answer = new int[2];
+        //가로x , 세로 y
         
+        int sum = brown + yellow;
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=1; i<sum/2; i++) {
             if(sum%i==0) {
-                list.add(i);
-                list.add(sum/i);
+                int x = Math.max(i, sum/i);
+                int y = sum / x;
+                map.put(x,y);
             }
         }
-        
-        dfs(0, answer); 
+        for(Integer x : map.keySet()) {
+            int y = map.get(x);
+            if(2*x + 2*y == (brown+4)){
+                answer[0] = x;
+                answer[1] = y;
+                break;
+            }
+        }
         
         return answer;
-    }
-    
-    boolean dfs(int depth, int[] answer) {
-        if(depth == 2) {
-            if(answer[0] >= answer[1] && (2*answer[0] + 2*(answer[1]-2)) == b) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        
-        for(Integer i : list) {
-            answer[depth] = i;
-            if(dfs(depth+1, answer)) return true;
-            answer[depth] = 0;
-        }
-        return false;
     }
 }
