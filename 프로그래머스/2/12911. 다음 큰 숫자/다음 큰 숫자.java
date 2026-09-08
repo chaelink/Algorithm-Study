3544cef549bfb90503ff90caca2ac4817fe070a3
권채린 <92051742+chaelink@users.noreply.github.com>
2026-09-08 11:36:13 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,30 +1,24 @@
 import java.util.*;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int count = Integer.bitCount(n);
-        int m = n+1;
+        String bi = Integer.toString(n,2);
+        int count = 0;
+        for(int i=0; i<bi.length(); i++) {
+            if(bi.charAt(i)=='1') count++;
+        }
         
         while(true) {
-            if(count == Integer.bitCount(m)) {
-                answer = m;
-                break;
-            } else {
-                m++;
+            n++;
+            String bin = Integer.toString(n, 2);
+            int count2 = 0;
+            for(int i=0; i<bin.length(); i++) {
+                if(bin.charAt(i)=='1') count2++;
+            }
+            if(count == count2) {
+                return n;
             }
         }
-        
-        return answer;
-    }
-    
-    //2진수 변환
-    public int cal(int n) {
-        int answer = 0;
-        while(n > 0) {
-            answer += n%2;
-            n = n/2;
-        }
-        return answer;
+      
     }
 }
