34a24bcadbf3207790e26f5f1630198def343580
권채린 <92051742+chaelink@users.noreply.github.com>
2026-09-08 11:19:19 +0900

    [level 2] Title: 짝지어 제거하기, Time: 64.99 ms, Memory: 66.1 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,27 +1,20 @@
 import java.util.*;
-
 class Solution
 {
     public int solution(String s)
     {
-        int answer = 1;
         Stack<Character> st = new Stack<>();
         st.push(s.charAt(0));
-        
         for(int i=1; i<s.length(); i++) {
-            
-            if(!st.isEmpty() && (st.peek() == s.charAt(i))) {
+            if(st.size()>0 && st.peek() == s.charAt(i)) {
                 st.pop();
             } else {
                 st.push(s.charAt(i));
             }
         }
         
-        if(st.isEmpty()) {
-            return 1;
-        } else {
-            return 0;
-        }
+        if(st.size()>0) {return 0;}
+        else return 1;
       
     }
 }
