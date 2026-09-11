0a1cf1d396fc09cf6616ba48ed67b389921fb25d
권채린 <92051742+chaelink@users.noreply.github.com>
2026-09-11 16:41:45 +0900

    [level 2] Title: 카펫, Time: 11.35 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,25 +2,25 @@ import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        //가로x , 세로 y
-        
         int sum = brown + yellow;
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=1; i<sum/2; i++) {
+        Map<Integer,Integer> set = new HashMap<>();
+        for(int i=sum; i>0; i--) {
             if(sum%i==0) {
-                int x = Math.max(i, sum/i);
-                int y = sum / x;
-                map.put(x,y);
+                int a = Math.max(i, sum/i);
+                set.put(a, sum/a);
             }
         }
-        for(Integer x : map.keySet()) {
-            int y = map.get(x);
-            if(2*x + 2*y == (brown+4)){
-                answer[0] = x;
-                answer[1] = y;
-                break;
+        
+        for(Integer a : set.keySet()) {
+            int b = set.get(a);
+            if((a-2)*(b-2) == yellow) {
+                answer[0] = a;
+                answer[1] = b;
+                return answer;
             }
         }
+        
+        
         
         return answer;
     }
