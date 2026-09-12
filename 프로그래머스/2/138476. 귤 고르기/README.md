4d8f4428fe784f89bc6495d5c1d7b73b30324e20
권채린 <92051742+chaelink@users.noreply.github.com>
2026-09-12 16:44:41 +0900

    [level 2] Title: 귤 고르기, Time: 27.87 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 93.5 MB, 시간: 86.99 ms
+메모리: 100 MB, 시간: 27.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 24일 21:17:14
+2026년 09월 12일 16:44:37
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -3,24 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-        //12233455
+        
+        //귤 사이즈, 갯수 저장
         Map<Integer, Integer> map = new HashMap<>();
+        
         for(int i=0; i<tangerine.length; i++) {
             map.put(tangerine[i], map.getOrDefault(tangerine[i],0)+1);
         }
         
-        List<Integer> list = new ArrayList<>(map.keySet());
-        list.sort((a,b) -> {
-            return map.get(b) - map.get(a);
-        });
-        
-        for(Integer i : list) {
-            if(k<=0) break;
-            
-            k = k - map.get(i);
+        List<Integer> list = new ArrayList<>(map.values());
+        list.sort(Collections.reverseOrder());
+        //갯수가 많은 순서대로 픽해서 k개 채우기
+        for(Integer num : list) {
+            k -= num;
             answer++;
+            if(k<=0) break;
         }
-        
+ 
         return answer;
     }
 }
