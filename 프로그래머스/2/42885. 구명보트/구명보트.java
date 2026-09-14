f46a2fd35edabe132af1cec8e8f43d762645fae9
권채린 <92051742+chaelink@users.noreply.github.com>
2026-09-14 22:05:57 +0900

    [level 2] Title: 구명보트, Time: 10.42 ms, Memory: 59.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,30 +1,21 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        Arrays.sort(people); //오름차순 정렬임
-        int start = 0;
+        //무조건 최대 2명씩
+        Arrays.sort(people);
+        
+        int start=0;
         int end = people.length-1;
         
-        //int[]를 그대로 사용? 삽입 삭제가 편한 자료구조로 옮겨서 구현?
-        
-        while(start<=end) {
-            if(start==end) {
-                answer++;
+        while(start <= end) {
+            if(people[start] + people[end]<=limit) {
                 start++;
-                end--;
-            } else {
-                if(people[start] + people[end] <= limit) {
-                    answer++;
-                    start++;
-                    end--;
-                } else {
-                    answer++;
-                    end--;
-                }
             }
+            answer++;
+            end--;
         }
+   
         return answer;
     }
 }
