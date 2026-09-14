41f752bc9392717ca43f29aaa07812fee9dd941b
권채린 <92051742+chaelink@users.noreply.github.com>
2026-09-14 23:41:28 +0900

    [level 2] Title: 디펜스 게임, Time: 48.78 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int k, int[] enemy) {
+        //일단 가능한만큼 병사로 막고, 병사가 부족해지는 순간
+        //과거 중 가장 큰 값 vs 현재 값 비교해서 더 적이 많은 곳에 무적권 사용
 
         int answer = 0;
         int m = enemy.length;
@@ -9,43 +11,17 @@ class Solution {
         PriorityQueue<Integer> pq =
                 new PriorityQueue<>(Collections.reverseOrder());
 
-        int i = 0;
-
-        while (i < m) {
-
-            // 디펜스 불가
-            if (n < enemy[i]) {
-                //무적권이 없으면 진행 불가
-                if(k<=0) break;
-
-               //이전에 처리한 디펜스값을 더해줘야 하는 경우
-                else if(!pq.isEmpty() && pq.peek()>enemy[i]) {
+        for(int e : enemy) {
+            pq.add(e);
+            n -= e;
+            answer++;
+            if(n<0) {
+                if(k==0) {
+                    return answer-1;
+                } else {
                     n += pq.poll();
                     k--;
-                    // i라운드 디펜스 가능
-                    if (n >= enemy[i]) {
-                        n -= enemy[i];
-                        pq.add(enemy[i]);
-                        answer = i+1;
-                        i++;
-                    }
-                    continue;
                 }
-                else if(pq.isEmpty() || pq.peek()<=enemy[i]) {
-                    //현재 라운드를 무적권 사용하는 경우
-                    k--;
-                    answer = i+1;
-                    i++;
-                    continue;
-                } 
-            }
-
-            // i라운드 디펜스 가능
-            if (n >= enemy[i]) {
-                n -= enemy[i];
-                pq.add(enemy[i]);
-                answer = i+1;
-                i++;
             }
         }
 
