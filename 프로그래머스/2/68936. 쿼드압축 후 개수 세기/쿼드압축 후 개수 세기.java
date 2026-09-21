a979202dc9e82c22682a3d87676c4efb8fbca3e9
권채린 <92051742+chaelink@users.noreply.github.com>
2026-09-21 18:28:07 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 17.92 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,49 +1,48 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[][] arr) {
-        int[] answer = new int[2]; //0,1개수
         int n = arr.length;
-        int size = n;
-        int[][] done = new int[n][n];
+        int[] answer = new int[2];
+        //방문 체크
+        int[][] visit = new int[n][n];
+        int idx = n;
         
-        while(size>1) {
-            for(int i=0; i<n; i+=size) {
-                for(int j=0; j<n; j+=size) {
-                    if(done[i][j]==0 && merge(i,j,size,arr)) {
-                        if(arr[i][j]==0) answer[0]++;
-                        else answer[1]++;
-                        for(int k=i; k<i+size; k++) {
-                            for(int l=j; l<j+size; l++) {
-                                done[k][l] = 1;
+        //큰 단위부터 보기(n부터 /2 1보다 클 떄까지)
+        while(idx>1) {
+            
+            for(int i=0; i<n; i+=idx) {
+                for(int ii=0; ii<n; ii+=idx) {
+                    if(visit[i][ii]==0 && check(i,ii, idx, arr)) {
+                        //answer 저장
+                        if(arr[i][ii]==0){answer[0]++;}
+                        else {answer[1]++;}
+                        //방문 처리
+                        for(int j=i; j<i+idx; j++) {
+                            for(int k=ii; k<ii+idx; k++) {
+                                visit[j][k]=1;
                             }
                         }
                     }
                 }
             }
-            size = size/2;
-            //왼쪽 위 지점 골라서
-            //합치기 가능한지 함수 돌리기
-            //가능하면 0이나 1 ++ 하고 해당 면적 완료 처리
-            
+            idx = idx/2;
         }
         
+        //방문안한 곳만 갯수 세서 answer에 저장
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                if(done[i][j]==0) {
-                    if(arr[i][j]==0) answer[0]++;
-                    else answer[1]++;
-                }
+                if(visit[i][j]==0) answer[arr[i][j]]++;
             }
         }
         
         return answer;
     }
     
-    boolean merge(int i, int j, int size, int[][] arr) {
-        int num = arr[i][j];
-        for(int k=i; k<i+size; k++) {
-            for(int l=j; l<j+size; l++) {
-                if(arr[k][l]!=num) return false;
+    boolean check(int start, int sta, int n, int[][] arr) {
+        int num = arr[start][sta];
+        for(int i=start; i<start+n; i++) {
+            for(int j=sta; j<sta+n; j++) {
+                if(num != arr[i][j]) return false;
             }
         }
         return true;
