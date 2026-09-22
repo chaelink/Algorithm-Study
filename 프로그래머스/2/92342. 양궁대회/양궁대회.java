7c422a5eca24d324860a408a65b0771b2cdf5a60
권채린 <92051742+chaelink@users.noreply.github.com>
2026-09-22 23:47:35 +0900

    [level 2] Title: 양궁대회, Time: 1.15 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -0,0 +1,70 @@
+import java.util.*;
+class Solution {
+    int[] answer = new int[11];
+    int diff=0;
+    int[] last = new int[11];
+    boolean onemore = false;
+    public int[] solution(int n, int[] info) {
+        
+        dfs(0,0,n,info);
+            
+        if(!onemore) {
+            int[] ans = {-1};
+            return ans;
+        }
+        
+        return last;
+    }
+    
+    void dfs(int round, int cnt, int n, int[] info) {
+        //2. 탈출 조건 - 화살이 남으면 0점에 배치 후 원복
+        if(round>10) {
+            if(cnt<n) answer[10] = (n-cnt);
+            //3. diff 계산
+            cal(info, answer);
+            answer[10] = 0;
+            return;
+        }
+        
+        //1. 선택지 탐색
+        if((n-cnt) > info[round]) {
+            answer[round] = info[round]+1;
+            //cnt += info[round]+1;
+            dfs(round+1, cnt + info[round]+1, n, info);
+            //cnt -= info[round]+1;
+            answer[round] = 0;
+        }
+        
+        dfs(round+1, cnt, n, info);      
+    }
+    
+    //승패 점수 계산 함수
+    void cal(int[] info, int[] answer) {
+        int api=0;
+        int ryn=0;
+        for(int i=0; i<11; i++) {
+            if(info[i]<answer[i]) {
+                ryn+=(10-i);
+            } else if(info[i]>=answer[i] && info[i]!=0) {
+                api+=(10-i);
+            }
+        }
+        if(ryn>api) {
+            onemore = true;
+            if((ryn-api)>diff) {
+                diff = ryn - api;
+                last = answer.clone();
+            }
+            else if((ryn-api)==diff) {
+                for(int i=10; i>=0; i--) {
+                    if(answer[i]>last[i]) {
+                        last = answer.clone();
+                    }
+                    if(answer[i] < last[i]) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
