50f6f236742fbd3f2cfea3353251e58d4228ec72
권채린 <92051742+chaelink@users.noreply.github.com>
2026-09-24 13:27:22 +0900

    [level 1] Title: 공원 산책, Time: 0.25 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -2,54 +2,68 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] park, String[] routes) {
         int[] answer = new int[2];
-        //로봇 강아지의 최종 위치 구하기
-        int w = park[0].length();
-        int h = park.length;
-        int[][] arr = new int[h][w];
-        //1. 배열 정리하기
-        for(int i=0; i<park.length; i++) {
+        int n = park.length; //세로
+        int m = park[0].length(); //가로
+        
+        //시작 위치 구하기
+        for(int i=0; i<n; i++) {
             String str = park[i];
-            for(int j=0; j<w; j++) {
-                if(str.charAt(j)=='O') {
-                    arr[i][j] = 1;
-                } else if(str.charAt(j)=='X') {
-                    arr[i][j] = 0;
-                } else if(str.charAt(j)=='S') {
-                    arr[i][j] = 1;
-                    answer[0] = i; answer[1] = j;
+            for(int j=0; j<str.length(); j++) {
+                if(str.charAt(j)=='S') {
+                    answer[0] = i;
+                    answer[1] = j;
                 }
             }
         }
         
-        //2. 루트 체크하기
-        for(int i=0; i<routes.length; i++) {
-            String[] str = routes[i].split(" ");
-            int d = Integer.parseInt(str[1]);
-            boolean check = true;
-            if(str[0].equals("N")) {
-                if(answer[0] - d<0) continue;
-                for(int j=1; j<=d; j++) {
-                    if(arr[answer[0] - j][answer[1]] == 0) check = false;
+        //루트 돌면서 가능하면 이동
+        for(String r : routes) {
+            String[] route = r.split(" ");
+            int d = Integer.parseInt(route[1]);
+            boolean go = true;
+            if(route[0].equals("N")) {
+                if(answer[0] - d>=0) {
+                    for(int i=answer[0]; i>=answer[0]-d; i--) {
+                        if(park[i].charAt(answer[1])=='X') {
+                            go = false;
+                            break;
+                        }
+                    }
+                    if(go) {answer[0] = answer[0]-d;}
                 }
-                if(check) answer[0] -= d;
-            } else if(str[0].equals("S")) {
-                if(answer[0] + d>=h) continue;
-                for(int j=1; j<=d; j++) {
-                    if(arr[answer[0] + j][answer[1]] == 0) check = false;
+            }
+            if(route[0].equals("S")) {
+                if(answer[0] + d<n) {
+                    for(int i=answer[0]; i<=answer[0]+d; i++) {
+                        if(park[i].charAt(answer[1])=='X') {
+                            go = false;
+                            break;
+                        }
+                    }if(go) {answer[0] = answer[0]+d;}
                 }
-                if(check) answer[0] += d;
-            } else if(str[0].equals("W")) {
-                if(answer[1] - d<0) continue;
-                for(int j=1; j<=d; j++) {
-                    if(arr[answer[0]][answer[1]-j] == 0) check = false;
+                
+            }
+            if(route[0].equals("W")) {
+                if(answer[1] - d>=0) {
+                    for(int i=answer[1]; i>=answer[1]-d; i--) {
+                        if(park[answer[0]].charAt(i)=='X') {
+                            go = false;
+                            break;
+                        }
+                    }if(go) {answer[1] = answer[1]-d;}
                 }
-                if(check) answer[1] -= d;
-            } else if(str[0].equals("E")) {
-                if(answer[1] + d>=w) continue;
-                for(int j=1; j<=d; j++) {
-                    if(arr[answer[0]][answer[1]+j] == 0) check = false;
+                
+            }
+            if(route[0].equals("E")) {
+                if(answer[1] + d<m) {
+                    for(int i=answer[1]; i<=answer[1]+d; i++) {
+                        if(park[answer[0]].charAt(i)=='X') {
+                            go = false;
+                            break;
+                        }
+                    }if(go) {answer[1] = answer[1]+d;}
                 }
-                if(check) answer[1] += d;
+                
             }
         }
         
